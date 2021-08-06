d960e279f3ffc7416235bb2057a03be49a792aa6
Leia Scofield <leia@leiascofield.com>
2021-08-06 15:11:37 -0500

    Display the first image from within the post content

---
+++ show-first-image.php
@@ -0,0 +1,10 @@
+// Get first image from post
+function PROJECTNAME_show_first_image() {
+	global $post, $posts;
+	$first_img = '';
+	ob_start();
+	ob_end_clean();
+	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+	$first_img = $matches [1][0];
+	return $first_img;
+}
